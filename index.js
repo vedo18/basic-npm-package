6db329ba9388acc04c85e32deb1398ede01f97ba
vedo18 <saketvats18@gmail.com>
2024-06-28 15:48:02 +0530

    fixed the issue

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require('express');
+const router = express.Router();
 
-const app = express();
-app.use(express.json());
-
-app.get('/health-check', (req, res) => {
-    res.send('Server running ');
+router.get('/health-check', (req, res) => {
+    res.send('Server running');
 });
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+module.exports = router;
